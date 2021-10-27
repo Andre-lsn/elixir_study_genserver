531e9864add155f4ef9cba074ab8a7ee4dc9af37
andre_nascimento <contato.nascimentoandre@gmail.com>
2021-10-27 17:44:36 -0300

    initialize the server responsible for the count

--- lib/count_facade/core/count.ex
+++ lib/count_facade/core/count.ex
@@ -1,33 +1,7 @@
 defmodule Core.Count do
-  use GenServer
-
-  @impl true
-  def init(value), do: {:ok, value}
-
-  @impl true
-  def handle_cast({:increment, value}, state) do
-    {:noreply, state + value}
-  end
-
-  @impl true
-  def handle_cast({:decrement, value}, state) do
-    {:noreply, state - value}
-  end
-
-  @impl true
-  def handle_cast({:reset}, _state) do
-    state = 0
-
-    {:noreply, state}
-  end
-
-  @impl true
-  def handle_call(:count, _from, state) do
-    {:reply, state, state}
-  end
 
   def start_link(init_arg \\ []) do
-    GenServer.start_link(__MODULE__, init_arg, name: __MODULE__)
+    GenServer.start_link(Core.CountServer, init_arg, name: __MODULE__)
   end
 
   def increment(value) when is_number(value) do
